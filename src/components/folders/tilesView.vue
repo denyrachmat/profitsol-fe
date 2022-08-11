93d5af96f3b10760c5680625bbd08ac960d75802
Deny Rachmat <denyrachmat22@gmail.com>
2022-08-11 20:13:08 +0700

    Feat: Add Folder View

---
+++ src/components/folders/tilesView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="full-height">
+    <div class="row">
+      <div
+        class="col-2 text-center cursor-pointer q-pa-md"
+        v-for="(file, idx) in files"
+        :key="idx"
+        @click="clickedFolder(file)"
+      >
+        <div>
+          <q-icon size="13em" color="cyan" name="folder" />
+        </div>
+        <div>
+          <span class="text-h5">{{ file.dfm_folder_name }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps, ref, onMounted, defineEmits } from "vue";
+
+const selectedFolder = ref([]);
+const folderList = ref([]);
+const files = ref([]);
+
+const props = defineProps({
+  data: Array,
+  // ...your custom props
+});
+
+onMounted(() => {
+  files.value = props.data;
+  folderList.value = props.data;
+});
+
+const clickedFolder = (val) => {
+  selectedFolder.value = val;
+  files.value = val.child_folders;
+};
+</script>
--- src/pages/DMS/uploadDocument.vue
+++ src/pages/DMS/uploadDocument.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="q-pa-md bg-grey">
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-2 text-right">
+        <q-btn-group spread flat>
+          <q-btn flat color="green" icon="upload" />
+          <q-btn flat color="accent" icon="visibility" />
+          <q-btn flat color="accent" icon="update" />
+        </q-btn-group>
+      </div>
+    </div>
     <div class="row">
       <div class="col">
-        <span class="text-h5">Folder</span>
+        <tilesView :data="folders" :selectFiles="onSelectFiles" />
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import apiRequest from "src/components/apiRequest";
+import { useAuthStore } from "src/stores/authStore";
 
-const choosedFolder = ref([]);
+import tilesView from "src/components/folders/tilesView.vue";
+
+const { postData } = apiRequest();
+const store = useAuthStore();
+const folders = ref([]);
+
+onMounted(() => {
+  getData();
+});
+
+const getData = async () => {
+  const data = await postData(
+    "get",
+    null,
+    `dms/documents/${store.authDet.username}`,
+    false,
+    false,
+    true
+  );
+
+  if (data) {
+    console.log(data.data);
+    folders.value = data.data;
+  }
+};
+
+const onSelectFiles = (val) => {
+  console.log(val);
+};
 </script>
